7371baf5fba2fa7ca9c6817c9810f4f2787ade21
Adan Jeronimo <adanjeronimo213@gmail.com>
2025-04-11 18:07:36 -0700

    Created a data structure for Teas.

---
+++ Files/data.js
@@ -0,0 +1,53 @@
+/**
+ * Here, I am creating my own array of objects for the tea bases.
+ * I am choosing 3 types of tea bases, Oolong, Black and Green. 
+ * For each base I will choose 3 types of teas.
+ * Each tea will have a name, type, and small discription. 
+ */
+const teaStructure = [
+    {
+        teaName : "Tie Guan Yin",
+        teaType : "Oolong",
+        teaDisc : "Floral, light, and airy - Often thought of as similar to an orchid."
+    },
+    {
+        teaName : "Jin Xuan",
+        teaType : "Oolong",
+        teaDisc : "Naturally milky, creamy, and buttery."
+    },
+    {
+        teaName : "Dan Cong",
+        teaType : "Oolong",
+        teaDisc : "Natural flavors and aroma - Full-bodied, rich, and fragrant."
+    },
+    {
+        teaName : "Matcha",
+        teaType : "Green",
+        teaDisc : "A fine powdered tea with a bright, emerald green color and a rich, vegetal taste."
+    },
+    {
+        teaName : "Jasmine",
+        teaType : "Green",
+        teaDisc : "Delicate, floral, and slightly sweet flavor, often enjoyed for its aromatic fragrance and refreshing taste."
+    },
+    {
+        teaName : "Hojicha",
+        teaType : "Green",
+        teaDisc : "Roasted green tea with a unique, earthy aroma and a distinct flavor profile."
+    },
+    {
+        teaName : "Earl Grey",
+        teaType : "Black",
+        teaDisc : "A black tea blend flavored with bergamot oil, a citrus fruit oil, resulting in a distinctive, citrusy, and often floral taste."
+    },
+    {
+        teaName : "Red Jade",
+        teaType : "Black",
+        teaDisc : "A deep, rich ruby-colored infusion with a complex flavor that includes notes of mint, cinnamon, and a subtle sweetness."
+    },
+    {
+        teaName : "Assam",
+        teaType : "Black",
+        teaDisc : "A robust, malty black tea variety known for its strong flavor and high caffeine content."
+    }
+]
